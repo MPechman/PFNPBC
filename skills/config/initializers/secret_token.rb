b089ba77414b7877d000015842920f524f37fa93
Michael Pechman <michael.m.pechman@gmail.com>
2013-10-06 13:21:03 -0700

    Added Rrails App

---
+++ skills/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skills::Application.config.secret_token = '2ee83709211afa9cfd986a5c7673d13d36315b5d3fb9481045fce62f70ce24751f52644bba6b145307004b8b84de7dd80473e6e61a87a0d336d0492b5eccbd30'
---
+++ skillster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillster::Application.config.secret_token = 'e6de6b4d3469db07fac6f2c7a8f3bc44363575d83d4e3469a62ba1eff38a84d007612dda25ce2a43a9d2fe0cc7b98208106eb42f85ca49cf7700c8d2699c2885'

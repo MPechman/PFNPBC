1d43c2b1a196f0756e600c53ba10c3a7416009a4
Michael Pechman <michael.m.pechman@gmail.com>
2013-10-06 15:19:41 -0700

    Created moneybags app

---
+++ moneybags/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneybags::Application.config.secret_token = '8d842cac9557fab79838ee2fded0f94c83174878930445922810bdf60412febb0de0ba5ad15dfb10b0d3b029fd0b6198a9d3b6e8717203f11552e6764449cf85'
